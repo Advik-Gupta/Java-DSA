ba3c094b89902b89cb2996339b5f9f001f34890c
Advik-Gupta <advikguptadev@gmail.com>
2025-12-08 00:03:44 +0530

    Updated answer to be more efficient

--- src/Q217.java
+++ src/Q217.java
@@ -1,17 +1,22 @@
+import java.util.HashSet;
+
 public class Q217 {
+
     public static boolean containsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
             }
+            seen.add(num);
         }
+
         return false;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,1};
+        int[] nums = {1, 2, 3, 1};
         System.out.println(containsDuplicate(nums));
     }
 }
